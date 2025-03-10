27006511ab158527ad0b2b9880e27bc98adc6e8b
Yurlov Sergey <sergey.yurlov.email@gmail.com>
2025-03-10 17:24:50 +0300

    first commit

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import "./index.css";
 import JSImage from "./assets/js.png";
 
 const mainTitle = document.createElement("h1");
-mainTitle.textContent = "I love JavaScript";
+mainTitle.textContent = "I love JavaScript!";
 const JSImageHTML = document.createElement("img");
 JSImageHTML.src = JSImage;
 
--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,7 +1,20 @@
+import babel from "@rollup/plugin-babel";
+import serve from "rollup-plugin-serve";
+import livereload from "rollup-plugin-livereload";
+import styles from "rollup-plugin-styles";
+import image from "@rollup/plugin-image";
+
 export default {
   input: "./index.js",
   output: {
     file: "./build/bundle.js",
     format: "cjs",
   },
+  plugins: [
+    babel({ babelHelpers: "bundled" }),
+    styles(),
+    image(),
+    serve({ open: true }),
+    livereload(),
+  ],
 };
